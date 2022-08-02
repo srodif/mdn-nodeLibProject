ae00d8e083d4c24b41e609fd14f0c1dd33e5861a
Srod <91968331+srodif@users.noreply.github.com>
2022-08-02 11:35:42 +0000

    quickfix

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -99,7 +99,7 @@ exports.author_delete_get = (req, res, next) => {
       Author.findById(req.params.id).exec(callback);
     },
     authors_books(callback) {
-      Book.find({'author: req.params.id'}).exec(callback);
+      Book.find({ 'author': req.params.id }).exec(callback);
     }
   }, (err, results) => {
     if (err) { return next(err); }
